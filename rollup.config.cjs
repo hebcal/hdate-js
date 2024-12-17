5379d900317fe76058819be32a1f9fdd661c2bfd
Michael J. Radwin <michael@radwin.org>
2024-12-17 07:46:42 -0800

    packaging: remove conditional from exports

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,12 +1,8 @@
-const {nodeResolve} = require('@rollup/plugin-node-resolve');
-const babel = require('@rollup/plugin-babel');
 const typescript = require('@rollup/plugin-typescript');
 const pkg = require('./package.json');
 const {defineConfig} = require('rollup');
 
 const banner = '/*! ' + pkg.name + ' v' + pkg.version + ' */';
-
-const TARGET_NODE_VER = '18.0.0';
 
 module.exports = defineConfig([
   {
@@ -27,23 +23,6 @@ module.exports = defineConfig([
         outDir: 'dist/esm',
         rootDir: './src',
       }),
-      babel({
-        babelHelpers: 'runtime',
-        plugins: ['@babel/plugin-transform-runtime'],
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              modules: false,
-              targets: {
-                node: TARGET_NODE_VER,
-              },
-            },
-          ],
-        ],
-        exclude: ['node_modules/**'],
-      }),
-      nodeResolve(),
     ],
     external: [/node_modules/],
   },
